41ef57fb8d7585b2319b76e19aa1e746211e757c
GalaXai <92019780+GalaXai@users.noreply.github.com>
2025-11-06 22:31:40 +0100

    chore: add types

--- adv24/02/02.ts
+++ adv24/02/02.ts
@@ -3,29 +3,27 @@ import { fetchData } from '../fetcher';
 // This is just reading the input_data
 const input = await fetchData('2024', '2');
 const lines = input.trim().split('\n');
-const numbers = lines.map(line => line.split(/\s+/).map(Number))
+const numbers: number[][] = lines.map(line => line.split(/\s+/).map(Number))
 
 
-const results = numbers.filter((number) => {
-    const result = number.slice(1).map((n,idx) => n - number[idx]);
-
-    return (result.every(n => n > 0) || result.every(n => n < 0))
-        && result.every(n => Math.abs(n) <= 3);
-})
-
-console.log(results.length, numbers.length);
-
-const isSafe = (diffs) => {
+const isSafe = (diffs: number[]): boolean => {
     return (diffs.every(n => n > 0) || diffs.every(n => n < 0))
         && diffs.every(n => Math.abs(n) <= 3 && n !== 0);
 };
 
+const results: number[][] = numbers.filter((number: number[]): boolean => {
+    const result = number.slice(1).map((n,idx) => n - number[idx]);
+
+    return isSafe(result);
+})
+
+console.log(results.length, numbers.length);
 
 // Part2
-const results2 = numbers.filter((number) => {
+const results2:number[][] = numbers.filter((number): boolean => {
   for (let i = 0; i < number.length; i++) {
-    const merged = [...number.slice(0, i), ...number.slice(i + 1)];
-    const result = merged.slice(1).map((n,idx) => n - merged[idx]);
+    const merged: number[] = [...number.slice(0, i), ...number.slice(i + 1)];
+    const result: number[] = merged.slice(1).map((n,idx) => n - merged[idx]);
     if (isSafe(result)) {
         return true;
     }
